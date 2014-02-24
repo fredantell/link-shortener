40a4337e0471aea9f06d9fafe4cf7966980d5a17
Fredrik Antell <fred@fredantell.com>
2014-02-24 14:12:50 -0800

    Add hiccup/templating to main page

--- src/link_shortener/core.clj
+++ src/link_shortener/core.clj
@@ -1,20 +1,39 @@
 (ns link-shortener.core
   (:use (compojure handler
                    [core :only (GET POST defroutes)]))
-  (:require [ring.adapter.jetty :as jetty]))
+  (:require
+   [link-shortener.layout :as layout]
+   [ring.util.response :as response]
+   [ring.adapter.jetty :as jetty]))
+
+(def counter (atom 10000))
+
+(def urls (atom {}))
+
+(defn shorten [url]
+  (let [id (swap! counter inc)
+        id (Long/toString id 36)]
+    (swap! urls assoc id url)))
+
+@urls
+(@urls "7pt")
+@counter
+(shorten "http://clojurebook.com")
+
 
 (defn app [request]
   {:status 200
    :body (with-out-str (print request))})
 
 (defn homepage [request]
-  (with-out-str (print request)))
+  (layout/common
+   [:p "hello"]))
 
 (defn redirect [id]
-  id)
+  (response/redirect (@urls id)))
 
 (defroutes app
   (GET "/" request (homepage request))
   (GET "/:id" [id] (redirect id)))
 
-(run)
+(jetty/run-jetty #'app {:port 8080 :join? false})
---
+++ src/link_shortener/layout.clj
@@ -0,0 +1,10 @@
+(ns link-shortener.layout
+  (:require [hiccup.page :refer [html5 include-css]]))
+
+(defn common [& body]
+  (html5
+    [:head
+     [:title "Welcome to guestbook"]
+     (include-css "/css/screen.css")]
+    [:body body]))
+
